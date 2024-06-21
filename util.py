365d169c96c0643abd595fa0d880b9d2335d3fee
JoaoLopez <joaolopez@id.uff.br>
2024-06-21 17:06:35 -0300

    Adding function to save the scripts output to log file

--- util.py
+++ util.py
@@ -7,3 +7,9 @@ def generate_data(data_size):
         key = xxhash.xxh128_hexdigest(str(value).encode('utf'))
         data[key] = value    
     return data
+
+def append_to_log_file(file_path, storage_type, data_size, exec_time):
+    with open(file_path, 'a') as file:
+        log_message = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+        log_message += f"Storage: {storage_type}\nData Size: {data_size}\nExecution time: {exec_time:.6f} seconds\n\n"
+        file.write(log_message)
