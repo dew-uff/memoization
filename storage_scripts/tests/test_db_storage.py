765a343ad3dd1906bbe7677eda5eb5f8eaa15f5a
JoaoLopez <joaolopez@id.uff.br>
2024-06-25 18:22:33 -0300

    Adding test script for the DBStorage class

--- storage_scripts/tests/test_db_storage.py
+++ storage_scripts/tests/test_db_storage.py
@@ -7,6 +7,27 @@ import sqlite3
 from DBStorage import DBStorage
 
 class TestDBStorage(unittest.TestCase):
+    def assert_data_correctly_inserted_on_db(self, expected_data):
+        query = "SELECT COUNT(*) FROM CACHE"
+        self.storage.cursor.execute(query)
+        count = self.storage.cursor.fetchone()[0]
+        self.assertEqual(count, len(expected_data))
+        
+        query = "SELECT key, value FROM CACHE"
+        self.storage.cursor.execute(query)
+        result = self.storage.cursor.fetchall()
+        aux = [(k, v) for k, v in expected_data.items()]
+        for res in result:
+            self.assertIn(res, aux)
+
+    def manually_insert_data(self, data):
+        query = "INSERT INTO CACHE(key, value) VALUES " + len(data) * "(?, ?), "
+        query = query[:-2]
+        params = []
+        for k, v in data.items():
+            params.append(k)
+            params.append(v)
+        self.storage.cursor.execute(query, params)
 
     def setUp(self):
         self.db_path = 'test_db.sqlite'
@@ -35,42 +56,44 @@ class TestDBStorage(unittest.TestCase):
                 'key3': 15.7}
         
         self.storage.persist_data(data)
-
-        query = "SELECT COUNT(*) FROM CACHE"
-        self.storage.cursor.execute(query)
-        count = self.storage.cursor.fetchone()[0]
-        self.assertEqual(count, 3)
+        self.assert_data_correctly_inserted_on_db(data)
         
-        query = "SELECT key, value FROM CACHE"
-        self.storage.cursor.execute(query)
-        result = self.storage.cursor.fetchall()
+        data_2 = {'key10': -1232.123,
+                  'key11': 346.435}
+        data.update(data_2)
+        self.storage.persist_data(data_2)
+        self.assert_data_correctly_inserted_on_db(data)
 
-        data_2 = [(k, v) for k, v in data]
-        for res in result:
-            self.assertIn(res, data_2)
-
-    ######TODO:!!!!
-    def test_restore_data(self):
-        # Test restoring all data
+    def test_restore_all_data(self):
+        data = {'key1': 10.5,
+               'key2': 20.3,
+               'key3': 15.7}
+        self.manually_insert_data(data)
         restored_data = self.storage.restore_all_data()
-        self.assertEqual(restored_data, data)
+        self.assertDictEqual(restored_data, data)
 
-        # Test restoring part of the data
-        keys_to_restore = ['key1', 'key3']
-        restored_partial_data = self.storage.restore_part_of_data(keys_to_restore)
-        expected_partial_data = {
-            'key1': 10.5,
-            'key3': 15.7
-        }
-        self.assertEqual(restored_partial_data, expected_partial_data)
+        data2 = {'key23': 1241.5,
+                 'key47': 216312}
+        data.update(data2)
+        self.manually_insert_data(data2)
+        restored_data = self.storage.restore_all_data()
+        self.assertDictEqual(restored_data, data)
 
-    def test_close_connection(self):
-        # Test closing the connection
-        self.storage.close_connection()
+    def test_restore_part_of_data(self):
+        data = {'key1': 10.5,
+                'key2': 20.3,
+                'key3': 15.7,
+                'key4': 112345.7,
+                'key5': 15.712312}
+        self.manually_insert_data(data)
+        restored_data = self.storage.restore_part_of_data(['key2', 'key5', 'key9'])
+        self.assertDictEqual(restored_data, {'key2': 20.3, 'key5': 15.712312})
 
-        # Attempting to perform an operation after closing should raise an exception
-        with self.assertRaises(sqlite3.ProgrammingError):
-            self.storage.restore_all_data()
+        restored_data = self.storage.restore_part_of_data(['key20', 'key25'])
+        self.assertDictEqual(restored_data, {})
+
+        restored_data = self.storage.restore_part_of_data(['key3'])
+        self.assertDictEqual(restored_data, {'key3': 15.7})
 
 if __name__ == '__main__':
     unittest.main()
