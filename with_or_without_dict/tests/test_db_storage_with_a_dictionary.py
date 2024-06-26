ae07e3a20054db6d39ecc836c5090cd6981511b4
JoaoLopez <joaolopez@id.uff.br>
2024-06-26 18:42:54 -0300

    Adding test suite for the DBStorageWithADictionary class

---
+++ with_or_without_dict/tests/test_db_storage_with_a_dictionary.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append('..')
+sys.path.append('../..')
+
+import unittest, os
+from unittest.mock import patch
+
+from DBStorageWithADictionary import DBStorageWithADictionary
+from storage_scripts.DBStorage import DBStorage
+
+class TestDBStorageWithADictionary(unittest.TestCase):
+    def setUp(self):
+        self.db_path = 'test_db.sqlite'
+        self.storage = DBStorageWithADictionary(self.db_path)
+
+    def tearDown(self):
+        self.storage.close_connection()
+        os.system(f'rm -rf {self.db_path}')
+
+    def test_instantiate_with_empty_dict(self):
+        os.system(f'rm -rf {self.db_path}')
+        
+        self.assertFalse(os.path.exists(self.db_path))
+        self.storage = DBStorageWithADictionary(self.db_path)
+        self.assertTrue(os.path.exists(self.db_path))
+        self.assertDictEqual(self.storage.dictionary, {})
+    
+    def test_restore_part_of_data_with_empty_dict(self):
+        args = ['key1', 'key2', 'key3']
+        expected_return = {'key1': 10.12, 'key2': False, 'key3': 'test'}
+        with patch.object(DBStorage, 'restore_part_of_data', return_value=expected_return) as superclass_restore_method:
+            self.assertDictEqual(self.storage.dictionary, {})
+            
+            data = self.storage.restore_part_of_data(args)
+            self.assertDictEqual(data, expected_return)
+            self.assertDictEqual(self.storage.dictionary, expected_return)
+            superclass_restore_method.assert_called_once_with(args)
+
+    def test_restore_part_of_data_using_dict(self):
+        args = ['key1', 'key2', 'key3']
+        expected_return = {'key1': 10.12, 'key2': False, 'key3': 'test'}
+        with patch.object(DBStorage, 'restore_part_of_data', return_value=expected_return) as superclass_restore_method:
+            self.storage.dictionary = {'key2': False, 'key3': 'test'} 
+            data = self.storage.restore_part_of_data(args)
+            self.assertDictEqual(data, expected_return)
+            self.assertDictEqual(self.storage.dictionary, expected_return)
+            superclass_restore_method.assert_called_once_with(['key1'])
+
+if __name__ == '__main__':
+    unittest.main()
+
