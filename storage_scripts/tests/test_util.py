61fb25922ac74a2ede190761223e8c4b4a2af50c
JoaoLopez <joaolopez@id.uff.br>
2024-06-26 08:21:48 -0300

    Updating util.py and its tests

--- storage_scripts/tests/test_util.py
+++ storage_scripts/tests/test_util.py
@@ -1,12 +1,15 @@
 import sys
 sys.path.append('..')
 
-import unittest, os
-from util import append_to_log_file
+import unittest, random, os
+from unittest.mock import patch
+from functools import wraps
+from util import append_to_log_file, measure_performance
 
 class TestUtil(unittest.TestCase):
     def setUp(self):
         self.log_file = 'test_log.txt'
+        self.num_repetitions = random.randint(3, 1000)
 
     def tearDown(self):
         os.system(f'rm {self.log_file}')
@@ -42,6 +45,41 @@ class TestUtil(unittest.TestCase):
                              f'Data Size: {data_size}\n'
                              f'Execution time: {exec_time:.6f} seconds\n\n')
         self.assert_log_file_content(expected_content)
+
+    def test_decorator(self):
+        self.call_count = 0
+
+        def count_main_function_calls(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                self.call_count += 1
+                return func(*args, **kwargs)
+            return wrapper
+    
+        @measure_performance(self.num_repetitions, self.log_file)
+        @count_main_function_calls
+        def aux(data):
+            return len(data)
+        
+        with patch('util.append_to_log_file') as append_func:
+            args = [1, 3, 5]
+            aux(args)
+            append_func.assert_called_once_with(self.log_file, 'TestUtil.test_decorator.<locals>.aux', len(args), len(args))
+        self.assertEqual(self.call_count, self.num_repetitions)
+
+    def test_decorator(self):
+        @measure_performance(self.num_repetitions, self.log_file)
+        def aux(data, x1, x2, x3, x4):
+            y1 = x1
+            y2 = x2
+            y3 = x3
+            y4 = x4
+            return len(data)
+        
+        with patch('util.append_to_log_file') as append_func:
+            args = [[1, 3, 5], 'test', False, -31.124, {1, 3, 2}]
+            aux(*args)
+            append_func.assert_called_once_with(self.log_file, 'TestUtil.test_decorator.<locals>.aux', 3, 3)
 
 if __name__ == '__main__':
     unittest.main()
--- storage_scripts/util.py
+++ storage_scripts/util.py
@@ -1,4 +1,4 @@
-import random, xxhash
+import random, xxhash, statistics
 
 def generate_data(data_size):
     data = {}
@@ -13,3 +13,16 @@ def append_to_log_file(file_path, storage_type, data_size, exec_time):
         log_message = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
         log_message += f"Storage: {storage_type}\nData Size: {data_size}\nExecution time: {exec_time:.6f} seconds\n\n"
         file.write(log_message)
+
+#The decorated function must return the execution time it wants to measure and its first argument ('data') must be the size of the input, so we have both variables we want to put on a chart. 
+def measure_performance(num_repetitions, log_file):
+    def decorator(func):
+        def wrapper(data, *args):
+            times = []
+            for _ in range(num_repetitions):
+                exec_time = func(data, *args)
+                times.append(exec_time)
+            median_time = statistics.median(times)
+            append_to_log_file(log_file, func.__qualname__, len(data), median_time)
+        return wrapper
+    return decorator
