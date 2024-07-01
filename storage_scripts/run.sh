fc3433068f462b961297b2888f5c6ab6a4f92c00
JoaoLopez <joaolopez@id.uff.br>
2024-07-01 19:58:58 -0300

    Adding initial sleep to run.sh script

--- storage_scripts/run.sh
+++ storage_scripts/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+sleep 10
 python persist_storage_script.py > temp_persist.txt 2>&1
 
 python restore_storage_script.py > temp_restore.txt 2>&1
 
-python map_log_file_2_table_file.py persist_storage_script_log.txt persist_output.txt 
+python map_log_file_2_table_file.py persist_storage_script_log.txt persist_output.txt
  
-python map_log_file_2_table_file.py restore_storage_script_log.txt restore_output.txt  
+python map_log_file_2_table_file.py restore_storage_script_log.txt restore_output.txt
