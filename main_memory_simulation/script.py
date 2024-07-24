f4beefee2e42d8b78a171bcfd5eba31758e8045b
JoaoLopez <joaolopez@id.uff.br>
2024-07-24 18:10:19 -0300

    Adding simulation script.py

---
+++ main_memory_simulation/script.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('..')
+
+import time
+# from speedupy.speedupy import initialize_speedupy, deterministic
+
+
+# @deterministic
+def pure_function(input, output):
+    time.sleep(1)
+    return output
+
+# @initialize_speedupy
+def main():
+    input = sys.argv[1:sys.argv.index('--exec-mode')]
+    
+    i = 0
+    data = []
+    while i < len(input):
+        data.append((input[i], float(input[i+1])))
+        i += 2
+        
+    for elem in data:
+        print(elem)
+        pure_function(elem[0], elem[1])
+
+main()
