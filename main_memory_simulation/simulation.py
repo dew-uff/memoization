f14e4f312be93f009788a75806f265aa0d4130c3
JoaoLopez <joaolopez@id.uff.br>
2024-08-09 17:47:46 -0300

    Doing little fixes on simulation.py

--- main_memory_simulation/simulation.py
+++ main_memory_simulation/simulation.py
@@ -71,8 +71,10 @@ def execute_simulation(all_data, num_dict):
 
     #Executing measuring time
     print('\nMeasuring time...')
+    sys.stdout.flush()
     generate_script_input_file(all_data)
     os.system(f'python script.py slow --exec-mode manual --num-dict {num_dict} -s db')
+    sys.stdout.flush()
 
     #Erasing the cache
     os.system(f'rm -rf .speedupy/')
@@ -86,36 +88,31 @@ def generate_script_input_file(data):
 
 ##TODO:TEST
 def main():
-    for i in range(NUM_SET_DRAWS):
+    drawn_config = draw_config()
+    for set, set_config in drawn_config.items():
         print('\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
-        print(f'Draw {i}!')
+        print(f'Running for {set}!')
+        for k, v in set_config.items(): print(f'{k}: {v}')
         print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
-        drawn_config = draw_config()
-        for set, set_config in drawn_config.items():
-            print('\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
-            print(f'Running for {set}!')
-            for k, v in set_config.items(): print(f'{k}: {v}')
-            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
-            data_size = int(set_config['cache_miss_rate'] * set_config['deterministic_calls'])
-            if data_size == 0:
-                data_size += 1
-                set_config['cache_miss_rate'] = data_size/set_config['deterministic_calls']
-                print(f"Ajustando cache_miss_rate: {set_config['cache_miss_rate']}")
-            all_data = generate_simulation_data(data_size, set_config['deterministic_calls'])
+        data_size = int(set_config['cache_miss_rate'] * set_config['deterministic_calls'])
+        if data_size == 0:
+            data_size += 1
+            set_config['cache_miss_rate'] = data_size/set_config['deterministic_calls']
+            print(f"Ajustando cache_miss_rate: {set_config['cache_miss_rate']}")
+        all_data = generate_simulation_data(data_size, set_config['deterministic_calls'])
 
-            for j in range(NUM_EXECUTION_OF_A_DRAW):
-                print(f'\n>>>>>>>>>>> Trial {j+1}')
-                execute_simulation(all_data, '0')
-                execute_simulation(all_data, '1')
-                execute_simulation(all_data, '2')
-                execute_simulation(all_data, '2-fast')
+        for j in range(NUM_EXECUTION_OF_A_DRAW):
+            print(f'\n>>>>>>>>>>> Trial {j+1}')
+            execute_simulation(all_data, '0')
+            execute_simulation(all_data, '1')
+            execute_simulation(all_data, '2')
+            execute_simulation(all_data, '2-fast')
 
-            os.system('rm input.txt')
+        os.system('rm input.txt')
 
 if __name__ == '__main__':
     NUM_EXECUTION_OF_A_DRAW = 10
-    NUM_SET_DRAWS = 100
     main()
 
 # Simulação escolhe valores aleatórios para as 2 variáveis.
